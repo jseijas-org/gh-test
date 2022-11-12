e51162876e31a4955432eb18cd30f70a088499ae
Julio Seijas <jus@microsoft.com>
2022-11-12 12:20:51 +0100

    initial commit

---
+++ iebank_api/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pa$$w0rd1990@localhost/iebank'
+db = SQLAlchemy(app)
+
+from iebank_api.models import Account
+
+db.create_all()
+
+from iebank_api import routes
