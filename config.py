2cad1a8506e8a180865a6d06ad8fd21f35e997e1
Julio Seijas <jus@microsoft.com>
2022-11-14 22:46:38 +0100

    back to normal config

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=os.getenv('DBUSER'),
     dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST') + ".postgres.database.azure.com",
+    dbhost=os.getenv('DBHOST'),
     dbname=os.getenv('DBNAME')
     )
     DEBUG = True
